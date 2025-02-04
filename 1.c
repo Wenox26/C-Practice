47c812712aba9ffbe19a1dfb2b8f53860ae26dea
Owen <owenoxj@gmail.com>
2025-02-04 08:11:23 +0800

    Update

--- 1.c
+++ 1.c
@@ -1,13 +1,22 @@
+//ARRAY EXERCISE
 #include <stdio.h>
 
-
-
 int main(){
-    int num1;
     
-    printf("Hello world: ");
-    
+    //Getting sum of int using array
+    int n[5], i, sum = 0;
 
+    printf("Enter numbers: ");
+    
+    for (i = 0; i < 5; i++){
+        scanf("%d", &n[i]);
+        sum += n[i]; //Sums the array elements
+    }
+
+    printf("The sum is: %d", sum);
+    
+    
+    
     return 0;
     
 }
