6602ab78716e5ebb64c884949157cb56b346fafd
Owen <owenoxj@gmail.com>
2025-02-04 08:36:29 +0800

    Update practice array

--- 1.c
+++ 1.c
@@ -3,18 +3,22 @@
 
 int main(){
     
-    //Getting sum of int using array
-    int n[5], i, sum = 0;
+    //Getting sum of int using array------
+    // int n[5], i, sum = 0;
 
-    printf("Enter numbers: ");
+    // printf("Enter numbers: ");
     
-    for (i = 0; i < 5; i++){
-        scanf("%d", &n[i]);
-        sum += n[i]; //Sums the array elements
-    }
+    // for (i = 0; i < 5; i++){
+    //     scanf("%d", &n[i]);
+    //     sum += n[i]; //Sums the array elements
+    // }
+    // printf("The sum is: %d", sum);
+    
+    //Size of------
+    int n[]= {1,2,3,4,5,};//Each element contains 4 bytes so 4x[5](number of elements inside the array)= 20
 
-    printf("The sum is: %d", sum);
-    
+    printf("Size of: %lu", sizeof(n));
+
     
     
     return 0;
