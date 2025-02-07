9486985b7e6215fe6d789d09a646a8d93add2acb
Owen <owenoxj@gmail.com>
2025-02-07 15:12:50 +0800

    Update

--- 1.c
+++ 1.c
@@ -18,7 +18,7 @@ int main(){
     int n[]= {1,2,3,4,5,};//Each element contains 4 bytes so 4x[5](number of elements inside the array)= 20
 
     printf("Size of: %lu", sizeof(n));
-
+    
     
     
     return 0;
