5be55a7a4cc660ac1be52d2a532a36e3add4ef09
Owen <owenoxj@gmail.com>
2025-02-22 10:10:46 +0800

    Update
    Fix problem not displaying the outputs diagonally. UPDATE[Used nested for-loop for the output]

--- JERUSALEM_LabAssignment.c
+++ JERUSALEM_LabAssignment.c
@@ -13,26 +13,28 @@ int main() {
 
     //User input
     printf("\nEnter %d values for a %dx%d matrix:\n", size * size, size, size);
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            int value;
-            printf("Element [%d][%d]: ", i, j);
-            scanf("%d", &value);
-            arr[i][j] = value * 50; // Multiply by 50 before storing
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                int value;
+                printf("Element [%d][%d]: ", i, j);
+                scanf("%d", &value);
+                arr[i][j] = value * 50; // Multiply by 50 before storing
+            }
         }
-    }
-    
+        
 
     // Displaying elements diagonally
     printf("\nDiagonal elements of the matrix:\n");
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i][i]);
-    }
-    printf("\n");
-    
-    printf("\nPROGRAMMED BY: OWEN M. JERUSALEM BSIT 1-D");
-    
-    
-    
+        for (int i = 0; i < size; i++) {
+            // Print spaces for alignment
+            for (int j = 0; j < i; j++) {
+                printf("   ");  // Three spaces for proper alignment
+            }
+            printf("%d\n", arr[i][size - i - 1]);  // Printing the diagonal element
+        }
+
+
+     printf("PROGRAMMED BY: OWEN M. JERUSALEM BSIT-1D");
+
     return 0;
 }
